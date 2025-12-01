b234fd81081a18dcd50355adb013b8b21eae3267
kkomiitlog <hamdannhurbrianne@gmail.com>
2025-12-02 01:34:44 +0800

    Fix swagger definition and update file paths

--- swagger-v2.js
+++ swagger-v2.js
@@ -42,7 +42,7 @@ const options = {
   swaggerDefinition,
   // IMPORTANT: Ensure these paths match the new file structure (no 'src/' prefix)
   apis: [
-    './routes/*.js', // Assuming your route files are here
+    './routes/studentRoutes.js', // Assuming your route files are here
     './models/*.js', 
     './index.js' // Change to index.js if that is your main server file
   ], 
@@ -53,5 +53,6 @@ const swaggerSpec = swaggerJSDoc(options);
 
 
 export default swaggerSpec;
+
 
 
